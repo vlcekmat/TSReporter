be9794d7edc0851e7f960eff26e943e5fa053473
vlcekmat <vlcekmat@seznam.cz>
2020-07-22 15:58:12 +0200

    Page switching system added to gui

--- ui_research.py
+++ ui_research.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter.font import Font
-import main
+#import main
 from threading import Thread
 from time import sleep
 
@@ -10,8 +10,8 @@ class ProgramThread(Thread):
 
     def run(self):
         if ProgramThread.instance_created is not True:
-            main.welcome_user()
-            main.main()
+            #main.welcome_user()
+            #main.main()
             ProgramThread.instance_created = False
 
     def set_instance_created(self):
@@ -19,55 +19,89 @@ class ProgramThread(Thread):
 
 
 class Application(Frame):
+    def __init__(self):
+        super().__init__()
+        page1 = self.MainMenu()
+        page_settings = self.SettingsMenu().pack_forget()
+
     color_theme = {
         1: '#4C95BB',  # Regular Buttons
         2: '#F4761D',  # Quit Button
         3: '#636363',  # Integrated Frames
-        4: '#4C4A49'   # Background
+        4: '#4C4A49'  # Background
     }
 
-    class MenuButton:
-        def __init__(self, text, frame, color1=None, color2=None,font_color='black'):
+    class AppButton:
+        def __init__(self, text, frame, color1=None, color2=None, font_color='black', command=None):
             if color1 is None:
                 color1 = Application.color_theme[1]
             if color2 is None:
                 color2 = Application.color_theme[1]
 
             my_font = Font(size=15)
-            report_bugs_button = Button(frame, text=text, height=1, width=10, bg=color1,
-                                        activebackground=color2,
-                                        padx=20, pady=20)
-            report_bugs_button['font'] = my_font
-            report_bugs_button.pack(padx=10, pady=10)
+            button = Button(frame, text=text, height=1, width=10, bg=color1,
+                            activebackground=color2, fg=font_color,
+                            padx=20, pady=20)
+            if command is not None:
+                button['command'] = command
+            button['font'] = my_font
+            button.pack(padx=10, pady=10)
 
-    def __init__(self):
-        super().__init__()
-        self.init_widgets()
+    class MainMenu(Frame):
+        def __init__(self):
+            super().__init__()
+            self.init_widgets()
 
-    def set_up_menu(self):
-        background_frame = Frame(self, bg=self.color_theme[4])
-        background_frame.pack(fill=BOTH, expand=True)
+        # region COMMANDS
+        def go_to_settings(self):
+            self.pack_forget()
+            Application.SettingsMenu().open_page()
+        # endregion
 
-        top_frame = Frame(background_frame, bg=self.color_theme[4])
-        top_frame.pack(side=TOP, fill=X)
+        def set_up_menu(self):
+            background_frame = Frame(self, bg=Application.color_theme[4])
+            background_frame.pack(fill=BOTH, expand=True)
 
-        left_frame = Frame(top_frame, bg=self.color_theme[3])
-        left_frame.pack(side=LEFT, pady=10, padx=10)
+            top_frame = Frame(background_frame, bg=Application.color_theme[4])
+            top_frame.pack(side=TOP, fill=X)
 
-        title_font = Font(size=20)
-        title = Label(left_frame, text='TSReporter',
-                      bg=self.color_theme[4], font=title_font, padx=17, pady=5, fg=self.color_theme[2])
-        title.pack(side=TOP)
+            left_frame = Frame(top_frame, bg=Application.color_theme[3])
+            left_frame.pack(side=LEFT, pady=10, padx=10)
 
-        report_button = self.MenuButton('Report Bugs', frame=left_frame)
-        batch_report_button = self.MenuButton('Batch Report', frame=left_frame)
-        settings_button = self.MenuButton('Settings', frame=left_frame)
-        quit_button = self.MenuButton('QUIT', color1=self.color_theme[2], color2=self.color_theme[2],
-                                      frame=left_frame, font_color='white')
+            title_font = Font(size=20)
+            title = Label(left_frame, text='TSReporter',
+                          bg=Application.color_theme[4], font=title_font, padx=17, pady=5, fg=Application.color_theme[2])
+            title.pack(side=TOP)
 
-    def init_widgets(self):
-        self.pack(fill=BOTH, expand=True)
-        self.set_up_menu()
+            report_button = Application.AppButton('Report Bugs', frame=left_frame)
+            batch_report_button = Application.AppButton('Batch Report', frame=left_frame)
+            settings_button = Application.AppButton('Settings', frame=left_frame, command=self.go_to_settings)
+            quit_button = Application.AppButton('QUIT', color1=Application.color_theme[2], color2=Application.color_theme[2],
+                                                frame=left_frame, font_color='white')
+
+        def init_widgets(self):
+            self.pack(fill=BOTH, expand=True)
+            self.set_up_menu()
+
+        def open_page(self):
+            self.pack()
+
+    class SettingsMenu(Frame):
+        def __init__(self):
+            super().__init__()
+            self.init_widgets()
+
+        # region COMMANDS
+        def go_to_main_menu(self):
+            self.pack_forget()
+            Application.MainMenu().open_page()
+        # endregion
+
+        def open_page(self):
+            self.pack()
+
+        def init_widgets(self):
+            button = Application.AppButton('BACK', frame=self, command=self.go_to_main_menu)
 
 
 root = Tk()
